89dc6251f22f3fa7410b475071fd4e28d0d50ee1
wtao <wtao@users-MBP-2.hsd1.ca.comcast.net>
2020-06-14 11:36:19 -0700

    Updates

---
+++ precache-manifest.638b7b9cd604aa99440d7edca068913b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4467e12eb603e327667ac8e50851e0e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c10d6f7798983ad5dccb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "811757cd5dabab623800",
+    "url": "/static/js/2.96b49174.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.96b49174.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c10d6f7798983ad5dccb",
+    "url": "/static/js/main.b7253631.chunk.js"
+  },
+  {
+    "revision": "141b7d42e3227beb65c8",
+    "url": "/static/js/runtime-main.94f71fc2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
